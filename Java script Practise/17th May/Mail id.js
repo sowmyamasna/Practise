9581438b7591e1b5463c34a2e2079e06d0c29171
sowmyamasna <sowmyamasna9@gmail.com>
2024-05-18 16:35:40 +0530

    done

--- Java script Practise/17th May/Mail id.js
+++ Java script Practise/17th May/Mail id.js
@@ -1,26 +1,44 @@
 function validateUser(){
 
-    let expectedUser = "sowmyamasna@gmail.com";
-    let expectedPwd = "123";
+    let expectedUser = "sowmyamasna@gmail.com";//variable-declaration and intialization//
+    let expectedPwd = "123";//variable-declaration and intialization//
 
    let username = document.getElementById("username").value;
    let password = document.getElementById("password").value;
+
+   console.log(typeof(password));
 
    console.log(username);
    console.log(password);
 
    if(username === expectedUser && password === expectedPwd){
 
-      
-    document.getElementById("message").innerHTML = "Login Success!!";
+    alert("Login succesfull")
+    document.getElementById("message").textContent = "Login Success!!";
    }
-   
+
+   else if(username != expectedUser && password == expectedPwd){
+    alert("Login failed.. Try Again")
+
+    document.getElementById("message").textContent = "Invalid username!!";
+
+   }
+   else if(username == expectedUser && password != expectedPwd){
+    alert("Login failed.. Try Again")
+
+    document.getElementById("message").textContent = "Invalid password!!";
+
+   }
 
     else{
-
+        alert("Login failed.. Try Again")
     document.getElementById("message").innerHTML = "Invalid Credentials!";
 
     }
+
+
+
+  
 
 
 }
